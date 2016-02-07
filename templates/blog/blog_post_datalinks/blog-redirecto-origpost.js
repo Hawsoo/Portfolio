36388b89b2785d90863e35ee3fe0b8d2bd2f4e1b
Hawsoo <Hawsoo@users.noreply.github.com>
2016-02-06 16:57:59 -0800

    Adds delay for fb scraper

---
+++ templates/blog/blog_post_datalinks/blog-redirecto-origpost.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    setTimeout(function () {
+        // Get blog post SKU
+        var url = window.location.pathname;
+        url = url.substring(url.lastIndexOf('/') + 1);
+        var filename = url.substring(0, url.indexOf('.'));
+        console.log(filename);
+
+        // Get name of working directory
+        var location = window.location.href;
+        var workingDir = location.substring(0, location.lastIndexOf('/'));
+
+        // Goto blog post
+        console.log(workingDir + '/../../../blog-post.html?post=' + filename);
+        window.location.href = workingDir + '/../../../blog-post.html?post=' + filename;
+    }, 500);
+});
